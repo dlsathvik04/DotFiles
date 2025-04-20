90d328eb7c3d0ee75fe140e18e5f57ba1073f1ec
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2025-04-20 19:44:47 +0530

    migrated from vscode snippets to luasnip

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,10 +1,6 @@
 return {
   "L3MON4D3/LuaSnip",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets/json" })
-      require("luasnip.loaders.from_lua").load({ paths = "./snippets/lua" })
-    end,
-  },
+  config = function()
+    require("luasnip.loaders.from_lua").load({ paths = { "./snippets/" } })
+  end,
 }
